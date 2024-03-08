3c9bf4c3c82f8db7dd84688e37e359d6857b200e
jonter <jonterrusian@gmail.com>
2024-03-08 13:26:37 +0300

    v 0.3 Complete Character Controller and sync anims

--- Fairy journey/Assets/Scripts/PlayerController.cs
+++ Fairy journey/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum PlayerState
+{
+    IDLE, 
+    WALK,
+    FALL,
+    CLIMB,
+    ATTACK,
+    DISABLED,
+    DEAD
+}
+
 public class PlayerController : MonoBehaviour
 {
     Rigidbody2D rb;
@@ -9,14 +20,21 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] float jumpSpeed = 8;
     CircleCollider2D legsCollider;
+    CapsuleCollider2D bodyCollider;
+    [SerializeField] float climbSpeed = 5;
+    float gravity;
     Animator anim;
+
+    public PlayerState state = PlayerState.IDLE;
 
     // Start is called before the first frame update
     void Start()
     {
+        bodyCollider = GetComponent<CapsuleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
         legsCollider = GetComponent<CircleCollider2D>();
         anim = GetComponent<Animator>();
+        gravity = rb.gravityScale;
     }
 
     // Update is called once per frame
@@ -25,7 +43,44 @@ public class PlayerController : MonoBehaviour
         SideMove();
         FlipChar();
         PlayerJump();
-        
+        Climb();
+        SetAnim();
+    }
+
+
+    void Climb()
+    {
+        rb.gravityScale = gravity;
+        LayerMask ladderLayer = LayerMask.GetMask("Ladders");
+        bool onLadder = bodyCollider.IsTouchingLayers(ladderLayer);
+        if (onLadder == false) return;
+        rb.gravityScale = 0;
+        float inputY = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(rb.velocity.x, inputY * climbSpeed);
+
+        LayerMask groundLayer = LayerMask.GetMask("Ground");
+        bool isGrounded = legsCollider.IsTouchingLayers(groundLayer);
+        if(isGrounded == false) state = PlayerState.CLIMB;
+    }
+
+    void SetAnim()
+    {
+        anim.enabled = true;
+        if (state == PlayerState.IDLE) anim.SetInteger("state", 0);
+        else if (state == PlayerState.WALK) anim.SetInteger("state", 1);
+        else if (state == PlayerState.FALL) anim.SetInteger("state", 2);
+        else if (state == PlayerState.CLIMB) StartCoroutine(SetClimbAnim());
+
+    }
+
+    IEnumerator SetClimbAnim()
+    {
+        anim.SetInteger("state", 3);
+        yield return null; 
+        if(rb.velocity.magnitude < 0.5)
+        {
+           anim.enabled = false;
+        }
     }
 
     void SideMove()
@@ -34,8 +89,8 @@ public class PlayerController : MonoBehaviour
         rb.velocity = new Vector2(inputX * speed, rb.velocity.y);
 
         float velX = Mathf.Abs(rb.velocity.x);
-        if (velX > 0.5f) anim.SetBool("walk", true);
-        else anim.SetBool("walk", false);
+        if (velX > 0.5f) state = PlayerState.WALK;
+        else state = PlayerState.IDLE;
     }
 
     void FlipChar()
@@ -61,6 +116,10 @@ public class PlayerController : MonoBehaviour
             {
                 rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
             }
+        }
+        else
+        {
+            state = PlayerState.FALL;
         }
 
     }
